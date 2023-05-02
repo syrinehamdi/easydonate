12df8d61acaac455f00d9caa37ce4652a3a23392
syrinehamdi <sirine.hamdi@esen.tn>
2023-05-02 23:41:23 +0100

    add services

---
+++ src/app/services/giveaway.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Giveaway } from './models/giveaway.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GiveawayService {
+
+  private baseUrl = 'http://localhost:8080/api/giveawayItems';
+
+  constructor(private http: HttpClient) { }
+
+  getAllGiveaways(): Observable<Giveaway[]> {
+    return this.http.get<Giveaway[]>(`${this.baseUrl}/list`);
+  }
+
+  getGiveawayById(id: number): Observable<Giveaway> {
+    return this.http.get<Giveaway>(`${this.baseUrl}/findById/${id}`);
+  }
+
+  createGiveaway(giveaway: Giveaway): Observable<Giveaway> {
+    return this.http.post<Giveaway>(`${this.baseUrl}/add`, giveaway);
+  }
+
+  updateGiveaway(id: number, giveaway: Giveaway): Observable<Giveaway> {
+    return this.http.put<Giveaway>(`${this.baseUrl}/modify/${id}`, giveaway);
+  }
+
+  deleteGiveaway(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/delete/${id}`);
+  }
+}
---
+++ src/app/services/models/giveaway.model.ts
@@ -0,0 +1,8 @@
+export class Giveaway {
+    id: number;
+    title: string;
+    description: string;
+    productName: string;
+    availability: boolean;
+    pickupLocation: string;
+}
